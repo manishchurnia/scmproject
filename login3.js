954db0058b350f40318f2fe88dbc7b765743731f
manishchurnia <manish1399.becse24@chitkara.edu.in>
2025-04-23 11:44:15 +0530

    login page js code improved

--- login3.js
+++ login3.js
@@ -2,12 +2,17 @@ const wrapper = document.querySelector('.wrapper');
 const signUpLink = document.querySelector('.signUp-link');
 const signInLink = document.querySelector('.signIn-link');
 
-signUpLink.addEventListener('click', () => {
-    wrapper.classList.add('animate-signIn');
-    wrapper.classList.remove('animate-signUp');
-});
+// Ensure all elements exist before adding listeners
+if (wrapper && signUpLink && signInLink) {
+    signUpLink.addEventListener('click', () => {
+        wrapper.classList.add('animate-signIn');
+        wrapper.classList.remove('animate-signUp');
+    });
 
-signInLink.addEventListener('click', () => {
-    wrapper.classList.add('animate-signUp');
-    wrapper.classList.remove('animate-signIn');
-});
+    signInLink.addEventListener('click', () => {
+        wrapper.classList.add('animate-signUp');
+        wrapper.classList.remove('animate-signIn');
+    });
+} else {
+    console.warn('One or more elements not found. Check class names.');
+}
